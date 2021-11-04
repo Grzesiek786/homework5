44bbf716f6f6a608866155d42c64e22c6c79bcd0
Grzegorz Sumara <gedokawa@MacBook-Pro-Grzegorz.local>
2021-11-04 19:21:48 +0100

    zadanie 5 z dyrektywa

--- src/app/shares/colors-status.directive.ts
+++ src/app/shares/colors-status.directive.ts
@@ -1,16 +1,24 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
-import { House } from '../interfaces/house';
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
+import { HouseStatus } from '../enums/house-status.enum';
 
 @Directive({
   selector: '[appColorsStatus]'
 })
 export class ColorsStatusDirective implements OnInit {
 
+  @Input() public houseStatus
+
   constructor(private render: Renderer2, private elementRef: ElementRef) {}
 
   ngOnInit(): void {
-    console.log(this.elementRef.nativeElement.querySelector('p .status'));
-    console.log(this.elementRef);
+    console.log(this.houseStatus === HouseStatus.FOR_RENT);
+    if(this.houseStatus === HouseStatus.FOR_RENT) {
+      this.render.setStyle(this.elementRef.nativeElement, 'color', 'red');
+    }
+
+    if(this.houseStatus === HouseStatus.SOLD) {
+      this.render.setStyle(this.elementRef.nativeElement, 'color', 'green');
+    }
   }
 
 }
